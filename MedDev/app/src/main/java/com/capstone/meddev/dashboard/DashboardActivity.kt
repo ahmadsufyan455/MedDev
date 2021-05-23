766620d431011d0e22f632e50b3bf95075ca93f3
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2021-05-23 08:57:30 +0700

    display username on dashboard

--- MedDev/app/src/main/java/com/capstone/meddev/dashboard/DashboardActivity.kt
+++ MedDev/app/src/main/java/com/capstone/meddev/dashboard/DashboardActivity.kt
@@ -3,10 +3,32 @@ package com.capstone.meddev.dashboard
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.meddev.R
+import com.capstone.meddev.databinding.ActivityDashboardBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashboardBinding
+    private lateinit var db: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val user = FirebaseAuth.getInstance().currentUser
+        db = FirebaseFirestore.getInstance()
+
+        val userId = user?.uid
+        if (userId != null) {
+            db.collection("users")
+                .document(userId)
+                .get()
+                .addOnSuccessListener { documentSnapshot ->
+                    val username = documentSnapshot.getString("username")
+                    binding.tvUsername.text = username
+                }
+        }
     }
 }

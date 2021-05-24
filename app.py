552583da039f19699ec2822c50ba1210acfd8939
janu-wra19 <83714457+janu-wra19@users.noreply.github.com>
2021-05-24 21:38:03 +0700

    update FastAPI for ML model
    update FastAPI for ML model

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from prediction import predict_desease, read_imagefile
+from prediction import predict, prepocess, read_image
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
-
-
 # 2. Create the app object
 app = FastAPI()  
 
@@ -12,14 +10,17 @@ def index():
     return {'message': 'Hello, World'}
 
 @app.post('/api/predict')
-def predict_image(file: bytes = File(...)):
-    #read the file
-    image = read_imagefile(file)
-    #predicton
-    prediction = predict_desease(image)
+async def predict_image(file: bytes = File(...)):
+    #read file uploaded
+    image = read_image(file)
+    #prepocessing image
+    image = prepocess(image)
+    #make prediction
+    prediction = predict(image)
     
     #print(prediction)
-    print("test")
+
+    return prediction
 
 
 if __name__ == '__main__':

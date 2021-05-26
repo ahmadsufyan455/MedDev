8bf04f766ce9eeb061db6ef8494ed8ad3917dda4
janu-wra19 <83714457+janu-wra19@users.noreply.github.com>
2021-05-26 23:53:14 +0700

    Fixed Method
    now the API can only fetch images from the directory (previously could upload images) and post the classification results

--- prediction.py
+++ prediction.py
@@ -1,9 +1,11 @@
 from io import BytesIO
+from sys import path
 from fastapi import responses
 import numpy as np
 import tensorflow as tf
 from PIL import Image, ImageOps
 import cv2
+import glob
 from tensorflow.keras.applications import imagenet_utils
 from tensorflow.keras.applications.imagenet_utils import decode_predictions
 
@@ -11,14 +13,14 @@ from tensorflow.keras.applications.imagenet_utils import decode_predictions
 model = None
 
 def load_model():
-    model = tf.keras.models.load_model('my_model.hdf5')
+    model = tf.keras.models.load_model('67.hdf5')
     print("Model loaded")
     return model
 
 _model = load_model()
 
-def read_image(image_encoded):
-    image = Image.open(BytesIO(image_encoded))
+def read_image():
+    image = Image.open('C:/Users/asus/Desktop/MedDev2.0/temp/cataract1.png')
     return image
 
 def prepocess(image: Image.Image):
@@ -35,13 +37,12 @@ def prepocess(image: Image.Image):
 
 
 def predict(image: np.ndarray):
-    class_names = ["Normal", "cataract", "glucauma", "eye desease"]
+    class_names = ["Normal", "cataract", "glaucoma", "eye desease"]
     predictions = _model.predict(image)
     predictions = np.random.randn (1, 4) # instead of the return value of predict ()
-    pred_labels = np.argmax (predictions, axis = -1)
+    pred_labels = np.argmax (predictions, axis =-1)
     #print (pred_labels) # [2 0 0 2 2 2 4 2 3 3]
     # When replaced with a label name
     pred_label_names = [class_names [x] for x in pred_labels]
-    #print (pred_label_names) # ['d', 'e', ​​'b', 'a', 'c', 'c', 'a', 'e', ​​'c', 'c']
     print(predictions)
     return pred_label_names

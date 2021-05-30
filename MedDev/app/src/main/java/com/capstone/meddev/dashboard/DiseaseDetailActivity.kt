c3699447300a13926fee011d91523415b561b9d9
ajiedr <ajiedibyor@gmail.com>
2021-05-30 12:41:40 +0700

    add disease detail activity

---
+++ MedDev/app/src/main/java/com/capstone/meddev/dashboard/DiseaseDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.capstone.meddev.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiseaseDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_disease_detail)
+    }
+}

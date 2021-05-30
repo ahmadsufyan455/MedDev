d2218d40473305d06d4ccafe05882c1e015ee83c
janu-wra19 <83714457+janu-wra19@users.noreply.github.com>
2021-05-30 18:11:24 +0700

    add article services
    add article servies

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
-from prediction import predict, prepocess, read_image
+from prediction import predict,prepocess, read_image
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File
+import json
+
+
 # 2. Create the app object
-app = FastAPI()  
+app = FastAPI()
 
+@app.get('/')
+def index():
+    return {'message': 'Server is Online'}
 
-@app.get("/")
-def predict_image():
-
-    #read file from dir
-    image = read_image()
-    #prepocessing image
-    image = prepocess(image)
-    #make prediction
-    prediction = predict(image)
-    
+@app.post("/submitform")
+async def Handle_form(eye_file : bytes = File(...)):
+    img = read_image(eye_file)
+    img = prepocess(img)
+    prediction = predict(img)
     print(prediction)
+    return {"Result": prediction}
 
-    return prediction
+@app.get("/article")
+def art():
+    # Opening JSON file
+    f = open('article.json',)
+    # returns JSON object as 
+    # a dictionary
+    data = json.load(f)
 
+    return data
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)

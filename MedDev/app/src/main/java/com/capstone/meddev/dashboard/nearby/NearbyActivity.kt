62b9b8585b3d1b4d4913e45c5a0c49849c4ec40f
ajiedr <ajiedibyor@gmail.com>
2021-06-02 12:01:04 +0700

    add nearby activity

---
+++ MedDev/app/src/main/java/com/capstone/meddev/dashboard/nearby/NearbyActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.meddev.dashboard.nearby
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.meddev.R
+
+class NearbyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nearby)
+    }
+}
